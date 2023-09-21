80e84087344c157df4f18e41554c1691de301d4f
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-09-21 09:16:19 +0900

    todoListの登録・更新・削除について実装完了

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -3,10 +3,12 @@ package com.example.todolist.controller;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,6 +17,7 @@ import com.example.todolist.form.TodoData;
 import com.example.todolist.repository.TodoRepository;
 import com.example.todolist.service.TodoService;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -22,6 +25,7 @@ import lombok.AllArgsConstructor;
 public class TodoListController {
     private final TodoRepository todoRepository;
     private final TodoService todoService;
+    private final HttpSession session;
 
     @GetMapping("/todo")
     public ModelAndView showTodoList(ModelAndView mv) {
@@ -31,26 +35,54 @@ public class TodoListController {
         return mv;
     }
 
+    @GetMapping("/todo/{id}")
+    public ModelAndView todoById(@PathVariable(name = "id") int id, ModelAndView mv) {
+        mv.setViewName("todoForm");
+        Todo todo = todoRepository.findById(id).get();
+        mv.addObject("todoData", todo);
+        session.setAttribute("mode", "update");
+        return mv;
+    }
+
     @GetMapping("/todo/create")
     public ModelAndView createTodo(ModelAndView mv) {
         mv.setViewName("todoForm");
         mv.addObject("todoData", new TodoData());
+        session.setAttribute("mode", "create");
         return mv;
     }
 
     @PostMapping("/todo/create")
-    public ModelAndView createTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result,
-            ModelAndView mv) {
+    public String createTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result,
+            Model model) {
 
         boolean isValid = todoService.isValid(todoData, result);
         if (!result.hasErrors() && isValid) {
             Todo todo = todoData.toEntity();
             todoRepository.saveAndFlush(todo);
-            return showTodoList(mv);
+            return "redirect:/todo";
         } else {
-            mv.setViewName("todoForm");
-            return mv;
+            return "todoForm";
         }
+    }
+
+    @PostMapping("/todo/update")
+    public String updateTodo(@ModelAttribute @Validated TodoData todoData, BindingResult result, Model model) {
+        boolean isValid = todoService.isValid(todoData, result);
+
+        if (!result.hasErrors() && isValid) {
+            Todo todo = todoData.toEntity();
+            todoRepository.saveAndFlush(todo);
+            return "redirect:/todo";
+        } else {
+            return "todoForm";
+        }
+    }
+
+    @PostMapping("/todo/delete")
+    public String deleteTodo(@ModelAttribute TodoData todoData) {
+        todoRepository.deleteById(todoData.getId());
+        return "redirect:/todo";
     }
 
     @PostMapping("/todo/cancel")
