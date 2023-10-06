4334d39565968094aafe4810f01e46141cf8a5db
shusuke-iruma-tea <shusuke.pana@gmail.com>
2023-10-06 16:04:50 +0900

    不要なインポート文を削除

--- src/main/java/com/example/todolist/controller/TodoListController.java
+++ src/main/java/com/example/todolist/controller/TodoListController.java
@@ -1,7 +1,5 @@
 package com.example.todolist.controller;
 
-import java.util.List;
-
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
